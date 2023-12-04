fc53982fed48d3dfd32c903aad126e65165bcf22
ilham-zukri <zukri.ilham@gmail.com>
2023-12-04 13:06:23 +0700

    fix(user): adding unique property to nim

--- database/migrations/2023_12_03_114444_add_nim_to_users.php
+++ database/migrations/2023_12_03_114444_add_nim_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('nim', 255)->after('id');
+            $table->string('nim', 255)->after('id')->unique();
         });
     }
 
